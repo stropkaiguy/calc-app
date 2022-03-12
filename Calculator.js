a25aa69e5e893120d11073c44b5b4fc37e8bc042
Nick Carothers <93961846+stropkaiguy@users.noreply.github.com>
2022-03-12 10:15:49 -0600

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,107 @@
+import { useState } from 'react';
+
+ function Calculator() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const ops = ['÷', '*', '+', '-', '.'];
+
+  const updateCalc = value => {
+    if (
+      ops.includes(value) && calc === ''  ||
+      ops.includes(value) && ops.includes(calc.slice(-1)
+      )
+    ) {
+      return;
+    }
+    setCalc(calc + value);
+
+    if (!ops.includes(value)) {
+      setResult(eval(calc + value).toString());
+    }
+  }
+  const createDigits = () => {
+    const digits = [];
+
+    for (let i = 1; i < 10; i++) {
+      digits.push(
+        <button onClick={() => updateCalc(i.toString())} key={i}>
+          {i}
+        </button>
+      )
+    }
+
+    return digits;
+  }
+
+  //Function for = button
+  const calculate = () => {
+    setCalc(eval(calc).toString());
+  }
+
+  //Function for DEL button
+  const deleteLast = () => {
+    if (calc == '') {
+      return;
+    }
+
+    const value = calc.slice(0, -1);
+
+    setCalc(value);
+
+    
+  }
+  //Function for AC button
+  const clearAll = () => {
+    if (calc == '' ) {
+      return;
+    }
+
+    const value = '';
+
+    setCalc(value);
+
+    setResult(value);
+    
+
+  }
+
+  
+
+  /* Need to create functions for
+    -AC: clear all button
+    -M: memory button
+    -M+: memory plus button*/
+    
+   return (
+     <div className ="App">
+       <div className="calculator">
+         <div className="display">
+           {result ? <span>({result})</span> : ''}&nbsp;
+           {calc || "0"}
+         </div>
+
+         <div className="operators">
+           <button onClick={() => updateCalc('÷')}>÷</button>
+           <button onClick={() => updateCalc('*')}>*</button>
+           <button onClick={() => updateCalc('+')}>+</button>
+           <button onClick={() => updateCalc('-')}>-</button>
+
+           <button onClick={deleteLast}>DEL</button>
+           <button onClick={clearAll}>AC</button>
+           <button>M</button>
+           <button>M+</button>
+         </div>
+
+         <div className="digits">
+           { createDigits() }
+           <button onClick={() => updateCalc('0')}>0</button>
+           <button onClick={() => updateCalc('.')}>.</button>
+           <button onClick={calculate}>=</button>
+         </div>
+       </div>
+     </div>
+   )
+ }
+
+ export default Calculator;
